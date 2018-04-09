9dbfdebe4859e342fe6ebbe8bb81292977f9a4ab
xwc18 <36760651+xwc18@users.noreply.github.com>
2018-04-09 15:41:26 +0700

    Update troll2.php

--- troll2.php
+++ troll2.php
@@ -1,1 +1,1 @@
-
+{"data":[{"id":"1","title_troll":"FIFA WORLD CUP 2014 VS FIFA WORLD CUP 2018","troll_img":"https://pics.me.me/fifa-world-cup-2014-ball-trollfootbal-fifa-world-cup-2018-28912246.png"}]}
