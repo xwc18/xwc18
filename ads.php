f4772dd70367b49b1704accb0afd3e736dba1e27
xwc18 <36760651+xwc18@users.noreply.github.com>
2018-04-15 10:48:49 +0700

    Update ads.php

--- ads.php
+++ ads.php
@@ -1,1 +1,1 @@
-{"link":"https:\/\/itunes.apple.com\/us\/app\/soi-cầu-miền-bắc\/id1361659714?ls=1&mt=8","image":"http:\/sieucauvip.online\/wp-content\/uploads\/2016\/07\/com.xoso_.ketqua.soicau-w250.png","title":"Tải ứng dụng soi cầu miền bắc"}
+{"link":"https:\/\/itunes.apple.com\/app\/id1361659714","image":"http:\/\/sieucauvip.online\/wp-content\/uploads\/2016\/07\/com.xoso_.ketqua.soicau-w250.png","title":"Tải ứng dụng soi cầu miền bắc"}
