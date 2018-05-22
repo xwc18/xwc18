e0657e19938729d14cc015638a9107813f1f35a9
xwc18 <36760651+xwc18@users.noreply.github.com>
2018-05-23 06:36:23 +0700

    Update troll2.php

--- troll2.php
+++ troll2.php
@@ -1,1 +1,1 @@
-{"data":[{"id":"1","title_troll":"FIFA WORLD CUP 2014 VS FIFA WORLD CUP 2018","troll_img":"https://pics.me.me/fifa-world-cup-2014-ball-trollfootbal-fifa-world-cup-2018-28912246.png"}]}
+{"data":[{"id":"1","title_troll":"Cảm xúc của vua bóng đá Đông Nam Á","troll_img":"http://cauthu.com.vn/wp-content/uploads/2018/01/troll-b%C3%B3ng-%C4%91%C3%A1-3.jpg"}]}
